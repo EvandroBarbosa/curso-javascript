e01122af9988aa217ffb89e04b9a38b038b3f01a
Evandro Barbosa <vandomarinho.vm@gmail.com>
2019-05-20 23:10:06 -0300

    Como adicionar comentario

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,14 @@
+//O JavaScript pode ser Organizado em Sentença de código
+//Que pode ou não terminar com ponto e virgula
+console.log("Sentença de código")
+
+//Também pode ser organizado em blocos de códigos
+//Que são um conjunto de senteça
+{
+    console.log("Ola");//pode finalizar com ponto e viergula, usar aspa dupla
+    console.log('Mundo!') //sem ponto e virgula, com aspa simples
+    {
+        console.log('E um bloco pode conter varias sentenças como blocos de codigos')
+    }
+}
+
